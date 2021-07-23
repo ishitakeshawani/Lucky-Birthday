7d3676f9a716261d2a735d33bef48ddd62c39a9b
ishitakeshawani <keshwaniishita054@gmail.com>
2021-07-23 17:40:57 +0530

    made header component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className="title">
+        <h1>Is Your Birthday Lucky?</h1>
+        <a href="#mainSection">scroll down to checkout</a>
+      </div>
+    );
+}
+
+export default Header;
